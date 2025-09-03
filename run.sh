9e7d596fd5ae871c2d5cad39f0cf35ccb2a73690
Alexander Averin <a.averin@rosintel.com>
2025-09-03 16:54:58 +0300

    update bash script

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
+echo "Create backend/data/bugs.json..."
 touch backend/data/bugs.json
 echo "[]" > backend/data/bugs.json
+
+echo "Install dependencies..."
+npm install
+
+echo "Install playwright browsers..."
+npx playwright install
+
+echo "START THE PROJECT..."
+npm run dev
